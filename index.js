d480e2c5f7b89304070d71c245a0570542fb0780
Rylox <in.telo3200@gmail.com>
2022-11-20 12:33:35 +0100

    initial commit

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (req, res) {
-	res.render('index', { title: 'Hey', message: 'Hello there!' })
+    res.render('index', {
+        redirectUrl: process.env.REDIRECT_URL,
+        title: process.env.TITLE,
+        text: process.env.TEXT,
+        buttonText: process.env.BUTTON_TEXT,
+        headText: process.env.HEAD_TEXT
+    })
 })
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
